90934fe8add43f617ae8e42d21b03ba39394ddc7
Maciej Górecki <maciekgorecki@icloud.com>
2021-07-02 23:12:08 +0200

    WIP: Removed file.

--- HomeGarden/Domain/Authentication/AuthCredentials.swift
+++
@@ -1,7 +0,0 @@
-//
-//  Created by Maciej Gorecki on 02/07/2021
-//
-
-import Foundation
-
-typealias AuthCredentials = (email: String, password: String)
--- HomeGarden/Domain/Authentication/Authenticator.swift
+++ HomeGarden/Domain/Authentication/Authenticator.swift
@@ -4,6 +4,8 @@
 
 import Foundation
 
+typealias AuthCredentials = (email: String, password: String)
+
 protocol Authenticator {
     func signin(using credentials: AuthCredentials) async throws
     func signup(using credentials: AuthCredentials) async throws
